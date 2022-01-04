0de3b6e936f07583784c8b14487f0c4caf68b9f4
Erfan Ahmed <erfan@therapservices.net>
2022-01-04 11:35:00 +0600

    Initial project structure

---
+++ src/main/java/com/example/apiUsage/ApiUsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiUsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiUsage/ApiUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiUsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
